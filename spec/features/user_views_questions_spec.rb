3ad5370e3cc7d8c968d7678e8458f6fc2aba5617
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-06-01 22:11:29 -0400

    initial setup and first user story done

---
+++ spec/features/user_views_questions_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "sees all questions" do
+  scenario "user sees questions" do
+    myquestion = Question.create(title: "Whats your favorite color?", description: "I'd like to know your favorite color, mine is red.")
+    visit questions_path
+    expect(page).to have_content(myquestion.title)
+  end
+end
