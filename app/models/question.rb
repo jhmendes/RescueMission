3c68ba30c4be48bbc50aeb447cf0be058de995d3
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-06-04 20:33:41 -0400

    added user id to question table and setup for answers model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   validates :title, presence: true, length: { minimum: 20}
   validates :description, presence: true, length: { minimum: 50}
+  # validates :user_id
+  # dont validate user id yet, finish building, add back in with user authentication
 end
---
+++ spec/features/user_answers_a_question_spec.rb
@@ -0,0 +1,23 @@
+# As a user
+# I want to answer another user's question
+# So that I can help them solve their problem
+#
+# Acceptance Criteria
+#
+# - I must be on the question detail page
+# - I must provide a description that is at least 50 characters long
+# - I must be presented with errors if I fill out the form incorrectly
+
+
+
+require 'rails_helper'
+
+feature "user answers a question" do
+  scenario "answers another users question to help solve their problem" do
+    # myquestion = Question.create(title: "Whats your favorite color?", description: "I'd like to know your favorite color, mine is red.")
+    # visit questions_path
+    # click_link "Whats your favorite color?"
+    # expect(page).to have_content("#{myquestion.id}.) Whats your favorite color? Description: I'd like to know your favorite color, mine is red." )
+
+  end
+end
