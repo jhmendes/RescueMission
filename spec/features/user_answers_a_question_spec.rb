5bc2ad6d06bde140a6e6c090c6c776206b05331c
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-06-05 21:55:35 -0400

    added tests for a user answering a question

--- spec/features/user_answers_a_question_spec.rb
+++ spec/features/user_answers_a_question_spec.rb
@@ -13,11 +13,30 @@
 require 'rails_helper'
 
 feature "user answers a question" do
-  scenario "answers another users question to help solve their problem" do
-    # myquestion = Question.create(title: "Whats your favorite color?", description: "I'd like to know your favorite color, mine is red.")
-    # visit questions_path
-    # click_link "Whats your favorite color?"
+  xscenario "answers another users question to help solve their problem" do
+
+    myquestion = Question.create(title: "Is Ruby difficult to learn?", description: "I hear Ruby is a simple language to learn but I am intimidated to start.  Any advice?  Does anyone know of any good online bootcamps that teach Ruby?")
+    user_answer = "Ruby is a great first time programming language!  The syntax is pretty straight forward and once you get the hang out it you can create some pretty cools apps, like this one."
+    visit questions_path
+    click_link "Whats your favorite color?"
+    fill_in 'Answer', with: user_answer
+    click_button 'Submit Answer'
     # expect(page).to have_content("#{myquestion.id}.) Whats your favorite color? Description: I'd like to know your favorite color, mine is red." )
+
+  end
+
+
+  xscenario "user unsuccessfully answers a question due to not writing a long enough description" do
+    # error for too short of a title and description
+    myquestion = Question.create(title: "Is Ruby difficult to learn?", description: "I hear Ruby is a simple language to learn but I am intimidated to start.  Any advice?  Does anyone know of any good online bootcamps that teach Ruby?")
+    user_answer = "Yes"
+    visit questions_path
+    click_link "Whats your favorite color?"
+    fill_in 'Answer', with: user_answer
+    click_button 'Submit Answer'
+
+    expect(page).to have_content("Answer is too short (minimum is 50 characters)")
+
 
   end
 end
